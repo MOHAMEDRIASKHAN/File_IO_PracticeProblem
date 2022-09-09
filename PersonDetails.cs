da581842d3aa32fe94289cc0d9ce9f0eef139c18
MOHAMEDRIASKHAN <108645671+MOHAMEDRIASKHAN@users.noreply.github.com>
2022-09-09 08:55:48 -0700

    UC_13__AddressBook Problem

---
+++ PersonDetails.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOPracticeProblem
+{
+    public class PersonDetails
+    {
+        public void ReadFromStreamReader(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "C:C:\\BridgeLabs\\Phase 2\\FileIOPracticeProblem\\FileIOPracticeProblem\\Details.txt";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        public void WriteFromStreamWriter(string path)
+        {
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Thank you");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,40 @@ namespace FileIOPracticeProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Problem ");
+
+            String path = @"C:\BridgeLabs\Phase 2\FileIOPracticeProblem\FileIOPracticeProblem\Details.txt";
+
+            PersonDetails fileOperation = new PersonDetails();
+            fileOperation.WriteFromStreamWriter(path);
+        }
+        public static void CheckFilesExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File is exists");
+            }
+            else
+            {
+                Console.WriteLine("File is not exists");
+            }
+        }
+        public static void ReadAllLinesFromFile(string path)
+        {
+
+
+            string[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+
+        public static void ReadAllTextFromFile(string path)
+        {
+            string line;
+            line = File.ReadAllText(path);
+
+            Console.WriteLine(line);
         }
     }
 }
-            
