f72a78b50ed075a2c07072834080c1a08994abdc
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-09 09:24:47 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace FileIOPracticeProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Problem ");
+        }
+    }
+}
+            
